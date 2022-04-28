39a11dc69c0d075f7bddbc784bbfda0fffbab63b
derapalne <derapalne@gmail.com>
2022-04-27 21:05:58 -0300

    Carritos Update

--- src/daosCarritos/carritosDaoArchivo.js
+++ src/daosCarritos/carritosDaoArchivo.js
@@ -1,35 +1,39 @@
 const ContenedorArchivo = require("../contenedores/contenedorArchivo");
+const check = require("../utils/check")
 
 class CarritosDaoArchivo extends ContenedorArchivo {
-
     constructor() {
-        super({fileName: "carritos"});
+        super({ fileName: "carritos" });
     }
 
     async agregarCarrito() {
         const carritos = await this.cargar();
         let id = 0;
-        if(carritos.length) {
-                id = carritos[carritos.length - 1].id + 1;
+        if (carritos.length) {
+            id = carritos[carritos.length - 1].id + 1;
         }
         const carrito = {
             id: id,
             timestamp: Date.now(),
             productos: [],
-        };   
+        };
         await carritos.push(carrito);
         await this.guardar(carritos);
         return carrito.id;
     }
 
     async agregarProducto(id, producto) {
-        const carritos = this.cargar();
-        const indexCarrito = carritos.findIndex((c) => c.id == id);
-        if(indexCarrito != -1) {
-            carritos[indexCarrito].productos.push(producto);
-            await this.guardar(carritos);
+        if (check(producto)) {
+            const carritos = this.cargar();
+            const indexCarrito = carritos.findIndex((c) => c.id == id);
+            if (indexCarrito != -1) {
+                carritos[indexCarrito].productos.push(producto);
+                await this.guardar(carritos);
+            } else {
+                return "Id inexistente.";
+            }
         } else {
-            return "Id inexistente.";
+            console.log("El producto no cumple los requisitos");
         }
     }
 
@@ -41,7 +45,6 @@ class CarritosDaoArchivo extends ContenedorArchivo {
         );
         await this.guardar(carritos);
     }
-
 }
 
 module.exports = CarritosDaoArchivo;
--- src/daosCarritos/carritosDaoMemoria.js
+++ src/daosCarritos/carritosDaoMemoria.js
@@ -3,27 +3,38 @@ const ContenedorMemoria = require("../contenedores/contenedorMemoria");
 class CarritosDaoMemoria extends ContenedorMemoria {
     constructor() {}
 
-    agregar(carrito) {
-        if(this.check(carrito)) {
-            super.guardar(carrito);
+    nuevoCarrito() {
+        let id = 0;
+        if (this.memoria.length) {
+            id = this.memoria[this.memoria.length - 1].id + 1;
+        }
+        const carrito = {
+            id: id,
+            timestamp: Date.now(),
+            productos: [],
+        };
+        this.memoria.push(carrito);
+        return carrito.id;
+    }
+
+    agregarProducto(idCart, producto) {
+        if (check(producto)) {
+            const indexCarrito = this.memoria.findIndex((c) => c.id == idCart);
+            if (indexCarrito != -1) {
+                this.memoria[indexCarrito].productos.push(producto);
+            } else {
+                return "Id inexistente.";
+            }
         } else {
             console.log("El producto no cumple los requisitos");
         }
     }
 
-    updateById(id, carrito) {
-        if(this.check(carrito)) {
-            for (let i = 0; i < this.memoria.length; i++) {
-                if (this.memoria[i].id == id) {
-                    carrito.id = id;
-                    carrito.timestamp = Date.now();
-                    this.memoria[i] = carrito;
-                    return carrito;
-                }
-            }
-        } else {
-            console.log("El producto no cumple los requisitos");
-        }
+    deleteProdById(id, idProd) {
+        const indexCarrito = this.memoria.findIndex((c) => c.id == id);
+        this.memoria[indexCarrito].productos = this.memoria[indexCarrito].productos.filter(
+            (e) => e.id != idProd
+        );
     }
 }
 
