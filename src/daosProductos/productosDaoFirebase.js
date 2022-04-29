b6bfeb4ef71c632195d3d17a704fe71cb433ee17
derapalne <derapalne@gmail.com>
2022-04-28 22:40:51 -0300

    Update productosDaoFirebase.js

--- src/daosProductos/productosDaoFirebase.js
+++ src/daosProductos/productosDaoFirebase.js
@@ -7,9 +7,41 @@ class ProductosDaoFirebase extends ContenedorFirebase {
     }
 
     async agregar(producto) {
+        if (check(producto)) {
+            try {
+                const productos = await this.getAll();
+                let id = 0;
+                if (productos.length) {
+                    id = productos[productos.length - 1].id + 1;
+                }
+                producto.id = id;
+                producto.timestamp = Date.now();
+                await this.guardar(producto);
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            return "El producto no cumple los requisitos";
+        }
+    }
+
+    async updateById(id, producto) {
         if(check(producto)) {
-            const productos = await this.getAll();
-            
+            try {
+                const doc = await this.query.doc(id);
+                await doc.update({
+                    nombre: producto.nombre,
+                    descripcion: producto.descripcion,
+                    codigo: producto.codigo,
+                    precio: producto.precio,
+                    thumbnail: producto.thumbnail,
+                    stock: producto.stock,
+                    timestamp: Date.now()
+                })
+                return id;
+            } catch (e) {
+                console.log(e);
+            }
         } else {
             return "El producto no cumple los requisitos"
         }
